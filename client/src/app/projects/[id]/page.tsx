b4681333ed5c339e0f6bc215fbb053ab68c21a35
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-09 02:26:20 +0530

    ProjectHeader made under projects

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,1 +1,28 @@
 // we made projects folder and for dynamic URL we are making this file under folder[id]
+"use client";
+import React, { useState } from "react";
+import ProjectHeader from "@/app/projects/ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  //this particular page grabs the id so that we can use that value  to grab it see below how to
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Baord"); //state for which of the tab items is active
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false); //new task modal is open or not
+
+  return (
+    <div>
+      {/* MODAL NEW TASK LEAVE A PLACEHOLDER FOR THIS */}
+      {/* Projectheader Component its taking activeTab status
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} /> */}
+      {/* {activeTab === "Board" && (
+        <Board/>
+      )} */}
+    </div>
+  );
+};
+
+export default Project;
