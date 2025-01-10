f39f0968639e230c3a851aab2ad079b985346af5
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-10 22:36:08 +0530

    Tabs done

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import { Clock, Filter, Grid, Grid3x3, List, Share2, Table } from "lucide-react";
+import { Share } from "next/font/google";
 import React, { useState } from "react";
 
 type Props = {
@@ -6,15 +8,79 @@ type Props = {
   setActiveTab: (tabName: string) => void;
 };
 
-const ProjectHeader = ({ activeTab, setActiveTab }: Props) => { //this state is in page.tsx 
-  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
+const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
+  //this state is in page.tsx
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false); //newproject pop-up
   return (
     <div className="px-4 xl:px-6">
       {/* MODAL NEW PROJECT */}
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <Header name="Product Design Development" />
+        {/* header compon imported. as we need it accross multiple pages. */}
+      </div>
+      {/* TABS */}
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center ">
+        <div className="flex flex-1 items-center gap-2 md:gap-4 ">
+          <TabButton
+            name="Board"
+            icon={<Grid3x3 className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Timeline"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </div>
+        {/* searchbar and share button on small screens we want that above our tabs */}
+        <div className="flex items-center gap-2">
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Filter className="h-5 w-5" />
+          </button>
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Share2 className="h-5 w-5" />
+          </button>
+          <div className="relative">
+            <input type="text" placeholder="Search Task" className="rounded-md border py-1 pl-10 focus:outline-none" />
+          </div>
+        </div>
+
       </div>
     </div>
+  );
+};
+type TabButtonProps = {
+  name: string;
+  icon: React.ReactNode;
+  setActiveTab: (isName: string) => void; //: The argument passed to this function is a string, which likely represents the name of the tab that should be set as active.
+  activeTab: string;
+};
+const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
+  const isActive = activeTab === name;
+  return (
+    <button
+      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-white sm:px-2 lg:px-4 ${
+        isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""
+      }`}
+      onClick={() => setActiveTab(name)}
+    >
+      {icon}
+      {name}
+    </button>
   );
 };
 
