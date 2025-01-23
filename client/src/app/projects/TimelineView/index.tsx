4359f91e634874d83a66ec59822dfde2347703d5
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-23 18:42:45 +0530

    Gantt-chart useMemo hook

--- client/src/app/projects/TimelineView/index.tsx
+++ client/src/app/projects/TimelineView/index.tsx
@@ -1,7 +1,7 @@
 import { useAppSelector } from "@/app/redux";
 import { useGetTasksQuery } from "@/state/api";
-import React, { useState } from "react";
-import { DisplayOption } from "gantt-task-react";
+import React, { useMemo, useState } from "react";
+import { DisplayOption, ViewMode } from "gantt-task-react";
 type Props = {
   id: string;
   setIsModalNewTaskOpen: (isOpen: boolean) => void;
@@ -15,7 +15,22 @@ const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
     error,
     isLoading,
   } = useGetTasksQuery({ projectId: Number(id) });
-  const [displayOptions, setDisplayOptions] = useState<DisplayOptions>({});
+  const [displayOptions, setDisplayOptions] = useState<DisplayOption>({
+    viewMode: ViewMode.Month,
+    locale: "en-US",
+  });
+  //   Using useMemo to update only when its needed
+  const ganttTask = useMemo(() => {
+    return tasks?.map((task) => ({
+      start: new Date(task.startDate as string),
+      end: new Date(task.dueDate as string),
+      name: task.title,
+      id: `Task-${task.id}`,
+      type: "task" as const, // Use "as const" to infer the literal type
+      progress: task.points ? (task.points / 10) * 100 : 0,
+      isDisabled: false,
+    }));
+  }, []); // Add dependencies array to ensure it updates when `tasks` changes
   return <div>Timeline</div>;
 };
 
