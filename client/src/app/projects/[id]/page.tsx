152503ab78ac0b1f48e58032817b4dbf9c2c36a4
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-23 17:06:39 +0530

    ListView initialize

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,11 +4,11 @@ import { useParams } from "next/navigation";
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
 
-// This file is a client component (because of "use client"), 
+// This file is a client component (because of "use client"),
 // so we use "useParams()" instead of receiving "params" as a prop.
 export default function Project() {
   // Grabs the route segment "id" from the URL, e.g. /projects/123 => id = "123".
-  const { id } = useParams() as { id: string };  
+  const { id } = useParams() as { id: string };
 
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
@@ -20,6 +20,9 @@ export default function Project() {
 
       {/* Only render the Board if the activeTab is "Board" */}
       {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
