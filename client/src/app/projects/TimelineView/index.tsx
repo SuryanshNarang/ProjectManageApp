5a8f4c14aa8ffcf947cbdab5d41c2e410f3fe71c
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-23 18:31:28 +0530

    Gantt-chart

--- client/src/app/projects/TimelineView/index.tsx
+++ client/src/app/projects/TimelineView/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-
+import { useAppSelector } from "@/app/redux";
+import { useGetTasksQuery } from "@/state/api";
+import React, { useState } from "react";
+import { DisplayOption } from "gantt-task-react";
 type Props = {
   id: string;
   setIsModalNewTaskOpen: (isOpen: boolean) => void;
 };
-
-const Timeline = (props: Props) => {
+type TaskTypeItems = "task" | "milestone" | "project";
+const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
+  // Not doing any kind of propDrilling here we are using our state for the first time
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const {
+    data: tasks,
+    error,
+    isLoading,
+  } = useGetTasksQuery({ projectId: Number(id) });
+  const [displayOptions, setDisplayOptions] = useState<DisplayOptions>({});
   return <div>Timeline</div>;
 };
 
