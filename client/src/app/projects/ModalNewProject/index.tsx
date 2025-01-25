026ac555d005113fa1c0bea7768f9d6119e0f730
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-25 10:55:28 +0530

    CreateNewProject Button rende

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,5 +1,5 @@
 import { useCreateProjectMutation } from "@/state/api";
-import Modal from "@/components/Model";
+import Modal from "@/components/Modal";
 import React, { useState } from "react";
 
 type Props = {
@@ -31,12 +31,25 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
     return projectName && description && startDate && endDate;
   };
   const inputStyles =
-    "w-full rounded border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:vg-dark-tertiary dark:text-white dark:focus-outline-none";
+    "w-full rounded border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:vg-dark-tertiary dark:text-white dark:focus-outline-none"; //used in the FORM WHEN NEWPROJCT pop-up is there
   // Import our Modal Component
   // see the usecase we now don't have to rewrite everything like styles and all etc.
-  return <Modal isOpen={isOpen} onClose={onClose} name="Create New Project">
-    
-  </Modal>;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} name="Create New Project">
+      {/* POP_UP With name and Close button created lets create a form to take inputs now.*/}
+      {/* Prevent default so that onsubmitting our page is not refreshed. */}
+      <form
+        action=""
+        className="mt-4 space-y-6"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" className={inputStyles} placeholder="Project Name" value={projectName}/>
+      </form>
+    </Modal>
+  );
 };
 
 export default ModalNewProject;
