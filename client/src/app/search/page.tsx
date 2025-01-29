39f81c21ec0f43e6953f92180d3033e8a13faf49
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-29 13:05:33 +0530

    SearchPage Input component div

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useSearchQuery } from "@/state/api";
 import { debounce } from "lodash";
 import Header from "@/components/Header";
+import TaskCard from "@/components/TaskCard";
 // No need of props as this is a new page for nextjs
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -32,6 +33,38 @@ const Search = () => {
           className="w-1/2 rounded border p-3 shadow"
           onChange={handleSearch}
         />
+      </div>
+      <div className="p-5">
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error occured while fetching search results</p>}
+        {!isLoading && !isError && searchResults && (
+          // this is where we add our search results
+          <div>
+            {searchResults.task && searchResults.task?.length > 0 && (
+              <h2>Tasks</h2>
+            )}
+            {searchResults.task?.map((task) => (
+              <TaskCard key={task.id} task={task} />
+            ))}
+
+            {/* FOR PROJECTS */}
+            {searchResults.project && searchResults.project?.length > 0 && (
+              <h2>Proejcts</h2>
+            )}
+            {searchResults.project?.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+
+            {/* USER  */}
+            {/* FOR PROJECTS */}
+            {searchResults.users && searchResults.users?.length > 0 && (
+              <h2>User</h2>
+            )}
+            {searchResults.users?.map((user) => (
+              <UserCard key={user.userId} user={user} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
