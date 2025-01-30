a2aed98c23bf6eea309f08ebe7b8ae5b6e138733
Suryansh Narang <suryanshnarang23@gmail.com>
2025-01-30 12:21:33 +0530

    Users Page initialize

---
+++ client/src/app/users/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// we made a backend for this. went in controller then router then index.
+type Props = {};
+
+const Users = (props: Props) => {
+  return <div>Users</div>;
+};
+
+export default Users
