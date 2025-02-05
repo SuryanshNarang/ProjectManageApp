6d22db63884d0fa1de801a279708587774654609
Suryansh Narang <suryanshnarang23@gmail.com>
2025-02-05 08:32:08 +0530

    UsersPage

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -1,12 +1,57 @@
+"use client";
 import { useGetUsersQuery } from "@/state/api";
 import React from "react";
 import { useAppSelector } from "../redux";
+import Header from "@/components/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
 // we made a backend for this. went in controller then router then index.
+
+const columns: GridColDef[] = [
+  { field: "userId", headerName: "ID", width: 100 },
+  { field: "username", headerName: "Username", width: 150 },
+
+  {
+    field: "profilePictureUrl",
+    headerName: "Profile Picture",
+    width: 100,
+    renderCell: (params) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="h-9 w-9">
+          <Image
+            src={`/${params.value}`}
+            alt={params.row.username}
+            width={100}
+            height={50}
+            className="h-full rounded-full object-cover"
+          />
+        </div>
+      </div>
+    ),
+  },
+];
 
 const Users = () => {
   const { data: users, isLoading, isError } = useGetUsersQuery();
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode); //redux toolkit makes it so easy see here again dark mode state is managed here.
-  return <div>Users</div>;
+  if (isLoading) return <div>Loading....</div>;
+  if (isError || !users) return <div>Error fetching users</div>;
+  return (
+    <div className="flex w-full flex-col p-8 ">
+      <Header name="Users" />
+      <div style={{ height: 650, width: "100%" }}>
+        <DataGrid
+          rows={users || []}
+          columns={columns}
+          getRowId={(row) => row.userId}
+          pagination
+          className={dataGridClassNames}
+          sx={dataGridSxStyles(isDarkMode)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Users;
