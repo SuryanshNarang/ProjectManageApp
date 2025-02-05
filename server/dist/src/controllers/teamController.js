901e4e9227c67aac55b84c0e0528382127bde2c1
Suryansh Narang <suryanshnarang23@gmail.com>
2025-02-05 09:09:19 +0530

    TeamsController

---
+++ server/dist/src/controllers/teamController.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getTeams = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient(); // Initialize Prisma Client
+// Endpoint to retrieve all users
+const getTeams = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const teams = yield prisma.team.findMany(); // Fetch all users from the database
+        const teamsWithUsernames = yield Promise.all(
+        //For each team, it performs an asynchronous operation to fetch data from the database.
+        teams.map((team) => __awaiter(void 0, void 0, void 0, function* () {
+            const productOwner = yield prisma.user.findUnique({
+                //for relations   
+                where: {
+                    userId: team.productOwnerUserId, //check schema we have a productOwnerUserId
+                }, //we are checking what the product user owner id is:
+                select: { username: true },
+            });
+        })));
+        res.json(teams); // Send users as JSON response
+    }
+    catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "Unknown error occurred";
+        res
+            .status(500)
+            .json({ message: `Error retrieving users: ${errorMessage}` });
+    }
+});
+exports.getTeams = getTeams;
---
+++ server/src/controllers/teamController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient(); // Initialize Prisma Client
+
+// Endpoint to retrieve all users
+export const getTeams = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const teams = await prisma.team.findMany(); // Fetch all users from the database
+    const teamsWithUsernames = await Promise.all(
+      //For each team, it performs an asynchronous operation to fetch data from the database.
+      teams.map(async (team: any) => {
+        const productOwner = await prisma.user.findUnique({
+          //for relations   
+          where: {
+            userId: team.productOwnerUserId!, //check schema we have a productOwnerUserId
+          }, //we are checking what the product user owner id is:
+          select: { username: true },
+        });
+      })
+    );
+    res.json(teams); // Send users as JSON response
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${errorMessage}` });
+  }
+};
