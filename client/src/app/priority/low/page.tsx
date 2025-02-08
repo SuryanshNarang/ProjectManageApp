163481fbc2e9d89dba9fd0faa1de26fec48dd12f
Suryansh Narang <suryanshnarang23@gmail.com>
2025-02-08 11:59:57 +0530

    Project Completed with all Priorities Page

---
+++ client/src/app/priority/low/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import ReusablePriorityPage from "../reusablePriorityPage";
+import { Priority } from "@/state/api";
+
+// These pages have a Addtask button where we can specify the projectID they are not referenced to anyProject so any Prioritytask is not in the context of any project so the user has to specify
+const Low = () => {
+  return <ReusablePriorityPage priority={Priority.Low} />;
+};
+
+export default Low;
+    
--- client/src/app/priority/urgent/page.tsx
+++ client/src/app/priority/urgent/page.tsx
@@ -2,9 +2,8 @@
 import React from "react";
 import ReusablePriorityPage from "../reusablePriorityPage";
 import { Priority } from "@/state/api";
-type Props = {};
 // These pages have a Addtask button where we can specify the projectID they are not referenced to anyProject so any Prioritytask is not in the context of any project so the user has to specify 
-const Urgent = (props: Props) => {
+const Urgent = () => {
   return <ReusablePriorityPage priority={Priority.Urgent} />;
 };
 
