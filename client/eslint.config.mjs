e8fa0827790ea6893fa3f59fb43b8c9d9f7f294a
Suryansh Narang <suryanshnarang23@gmail.com>
2025-03-15 16:29:10 +0530

    fixes

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,15 +2,16 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   plugins: ['@typescript-eslint'],
   env: {
     browser: true,
     node: true,
+    es2020: true,  // Add support for ES2020 and later
   },
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: 'module', // Ensure this is set for ES modules
   },
 };
