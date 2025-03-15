8371a41de82bd573c5386b1d6812710088e10f40
Suryansh Narang <suryanshnarang23@gmail.com>
2025-03-15 15:33:35 +0530

    .js

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  experimental: {
-    isrMemoryCacheSize: 50 * 1024 * 1024, // 50MB cache
-  },
 };
 
 export default nextConfig;
