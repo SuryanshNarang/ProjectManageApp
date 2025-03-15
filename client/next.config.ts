5a62de1daee4b3ce5365647457bbd799cad8af85
Suryansh Narang <suryanshnarang23@gmail.com>
2025-03-15 14:00:29 +0530

    Fixes timeline

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   trailingSlash: true,
+  distDir: "out", // Ensures build output is consistent
 };
 
 export default nextConfig;
